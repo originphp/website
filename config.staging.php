e22f3a876c1bf5734615d5992c0ea548a1522cc8
Jamiel <20553479+jamiel-sharief@users.noreply.github.com>
2019-05-07 16:58:57 +0200

    no message

--- config.staging.php
+++ config.staging.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'baseUrl' => 'http://jigsaw-docs-staging.tighten.co',
+    'baseUrl' => 'https://beta.originphp.com',
     'production' => false,
 
     // DocSearch credentials
