a3533a07c9ef2cc2171e98a7e059efdc4536f75c
Jamiel Sharief <20553479+jamielsharief@users.noreply.github.com>
2021-04-16 08:37:05 +0200

    Adjusted year generation

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -96,7 +96,7 @@
         <footer class="bg-white text-center text-sm mt-12 py-4" role="contentinfo">
             <ul class="flex flex-col md:flex-row justify-center list-reset">
                 <li class="md:mr-2">
-                    &copy; Copyright 2018 - 2020 Jamiel Sharief.
+                    &copy; Copyright 2018 - <?= date('Y') ?> Jamiel Sharief.
                 </li>
 
                 <!--li>
